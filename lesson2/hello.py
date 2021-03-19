4f1e820d45f3d3f1e89f8d6ae31125c1e9737655
Mai Phong <phong.mt184172@gmail.com>
2021-03-19 22:47:54 +0700

    Revert "Generate private and public key and explaination of nonce"
    This reverts commit 9adc2a844df6683dd62b898330ce1cfaf36f4b85.

--- lesson2/hello.py
+++
@@ -1,8 +0,0 @@
-from cryptotools.BTC import generate_keypair, push, script_to_address, OP
-
-private, public = generate_keypair()
-print(private.hex())
-print(public.hex())
-print(public.to_address('P2PKH'))
-
-

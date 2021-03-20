c2df2440c9658e3ea91f82e554827f412a68e723
namnguyenn99 <namnd.vnist@gmail.com>
2021-03-20 22:38:17 +0700

    Generate Private Key, Public Key, Address with C# & .NET

---
+++ nguyenducnam-hust/lesson2/C#/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using NBitcoin;
+
+namespace lesson2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var privateKey = new Key();
+
+            var bitcoinPrivateKey = privateKey.GetWif(Network.Main);
+
+            var bitcoinPublicKey = bitcoinPrivateKey.PubKey;
+
+            var address = bitcoinPublicKey.GetAddress(ScriptPubKeyType.Segwit, Network.Main);
+
+            Console.WriteLine("Private Key: " + bitcoinPrivateKey);
+            Console.WriteLine("Public Key: " + bitcoinPublicKey);
+            Console.WriteLine("Address: " + address);
+        }
+    }
+}
